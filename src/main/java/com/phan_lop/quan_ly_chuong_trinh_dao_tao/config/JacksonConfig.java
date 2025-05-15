9a19ccf95c417eda7a3778e12183a53b1d82c90b
tieep <tn5271291@gmail.com>
2025-05-15 13:14:52 +0700

    backup

--- src/main/java/com/phan_lop/quan_ly_chuong_trinh_dao_tao/config/JacksonConfig.java
+++ src/main/java/com/phan_lop/quan_ly_chuong_trinh_dao_tao/config/JacksonConfig.java
@@ -1,9 +1,6 @@
 package com.phan_lop.quan_ly_chuong_trinh_dao_tao.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
